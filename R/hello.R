1357298e704b2070efc238a77a57a92af0b32163
Rlisbeth <karinalisbethmanyailvis@gmail.com>
2024-07-17 17:51:56 -0500

    kar.R

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
+plot_pie_chart <- function(labels, sizes, title = "Gráfico de Pastel") {
+  # Crear el gráfico de pastel con la función pie()
+  pie(sizes, labels = labels, main = title, col = rainbow(length(labels)))
 }
